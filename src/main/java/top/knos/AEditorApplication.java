83626873b25afd46e46b3e3cc0cb8f4471daa1a0
guwan <wu6789@qq.com>
2016-01-10 23:50:22 +0800

    init import

---
+++ src/main/java/top/knos/AEditorApplication.java
@@ -0,0 +1,12 @@
+package top.knos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AEditorApplication.class, args);
+	}
+}
