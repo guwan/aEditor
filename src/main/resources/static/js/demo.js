9b9913024442e669b794be7d948ea63059b67b75
guwan <wu6789@qq.com>
2016-01-13 18:01:22 +0800

    remove mvcConfig.java

--- src/main/resources/static/js/demo.js
+++ src/main/resources/static/js/demo.js
@@ -1,7 +1,4 @@
-$('#medium-editor-submit').click(function(){
-	event.preventDefault();//使a自带的方法失效
-    $.post('*', {'nike':0},
-    function(cdata) {
-        console.log('ok', cdata)
-    })
-})
+//$('#medium-editor-submit').click(function(){
+//	event.preventDefault();//使a自带的方法失效
+//	$('#medium-editor-submit')
+//})
